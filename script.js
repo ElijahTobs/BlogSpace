826f53ff60fc0d9d4de81848c53f1855486747ad
Elijah <eligenius@gmail.com>
2022-10-10 09:35:23 +0100

    Refactor javascript by adding displayPosts function

--- script.js
+++ script.js
@@ -1,43 +1,45 @@
+let postsArr = []
+
+function displayPosts() {
+  let html = ""
+  for (let post of postsArr) {
+      html += `
+      <h3>${post.title}</h3>
+      <p>${post.body}</p>
+      <hr />
+    `
+  }
+  document.getElementById("blog-list").innerHTML = html
+}
+
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
     .then(data => {
-        const postsArr = data.slice(0, 5)
-        let html = ""
-        for (let post of postsArr) {
-            html += `
-                <h3>${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-            `
-        }
-        document.getElementById("blog-list").innerHTML = html
+        postsArr = data.slice(0, 5)
+        displayPosts()
     })
 
 document.getElementById("new-post").addEventListener("submit", function(e) {
-    e.preventDefault()
-    const postTitle = document.getElementById("post-title").value
-    const postBody = document.getElementById("post-body").value
-    const data = {
-        title: postTitle,
-        body: postBody
-    }
-    
-    const options = {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }
-    
-    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
-        .then(res => res.json())
-        .then(post => {
-            document.getElementById("blog-list").innerHTML = `
-                <h3>${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-                ${document.getElementById("blog-list").innerHTML}
-            `
-        })
+  e.preventDefault()
+  const postTitle = document.getElementById("post-title").value
+  const postBody = document.getElementById("post-body").value
+  const data = {
+      title: postTitle,
+      body: postBody
+  }
+  
+  const options = {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+          "Content-Type": "application/json"
+      }
+  }
+  
+  fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+      .then(res => res.json())
+      .then(post => {
+        postsArr.unshift(post)
+        displayPosts()
+      })
 })
